da3618e2b1b66066ab55c28c618ef5d11aad6319
Elias Ahlers <elias@ahlers.click>
2024-09-21 22:30:07 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Course, Conflict, ScheduleItem } from './types';
 import './App.css';
-
-const courses: Course[] = [
-	{ id: 1, name: "Empirical Security Analysis and Engineering", instructor: "Holz", domain: "PI", semester: "WiSe 24/25", cp: 6, schedule: "Mo 12-14, Mi 12-14" },
-	{ id: 2, name: "Visualisierung", instructor: "Linsen", domain: "PI", semester: "WiSe 24/25", cp: 9, schedule: "Di 10-12, Fr 10-12", tutorial: "Mi 10-12" },
-	{ id: 3, name: "Computer Vision", instructor: "Jiang", domain: "PI", semester: "WiSe 24/25", cp: 9, schedule: "Mo 10-12, Do 10-12" },
-	{ id: 4, name: "Deep Reinforcement Learning", instructor: "Schilling", domain: "PI", semester: "WiSe 24/25", cp: 6, schedule: "Di 10-12, Fr 10-12", tutorial: "Mi 10-12" },
-	{ id: 5, name: "Verteilte Systeme", instructor: "Gorlatch", domain: "PI", semester: "WiSe 24/25", cp: 6, schedule: "Di 16-18", tutorial: "Mi 16-18" },
-	{ id: 6, name: "Algorithmische Geometrie", instructor: "Varenhold", domain: "FM", semester: "WiSe 24/25", cp: 9, schedule: "Mo 12-14, Do 12-14", tutorial: "Fr 10-12" },
-	{ id: 7, name: "Modellierung und Analyse von dynamischen Systemen", instructor: "Remke", domain: "FM", semester: "WiSe 24/25", cp: 9, schedule: "Di 12-14, Fr 12-14", tutorial: "Do 12-14" },
-	{ id: 8, name: "Theorie der Programmierung", instructor: "Müller-Olm", domain: "FM", semester: "SoSe 25", cp: 9 },
-	{ id: 9, name: "Aus dem Bereich Algorithmik", instructor: "", domain: "FM", semester: "SoSe 25", cp: 6 },
-	{ id: 10, name: "Quantitatives Model Checking", instructor: "Remke", domain: "FM", semester: "SoSe 25", cp: 6 },
-	{ id: 11, name: "Parallele Systeme", instructor: "Gorlatch", domain: "PI", semester: "SoSe 25", cp: 9 },
-	{ id: 12, name: "Visual Analytics", instructor: "Linsen", domain: "PI", semester: "SoSe 25", cp: 6 },
-	{ id: 13, name: "Algorithmik", instructor: "", domain: "FM", semester: "WiSe 25/26", cp: 9 },
-	{ id: 14, name: "Automated Planning and Acting", instructor: "Braun", domain: "FM", semester: "WiSe 25/26", cp: 6 },
-	{ id: 15, name: "Lambda-Kalkül und funkt. Sprachen", instructor: "Lammers", domain: "FM", semester: "WiSe 25/26", cp: 6 },
-	{ id: 16, name: "Computer Vision", instructor: "Jiang", domain: "PI", semester: "WiSe 25/26", cp: 9 },
-	{ id: 17, name: "Deep Reinforcement Learning", instructor: "Schilling", domain: "PI", semester: "WiSe 25/26", cp: 6 },
-	{ id: 18, name: "Sicherheit in eingebetteten Systemen", instructor: "Herber", domain: "PI", semester: "WiSe 25/26", cp: 6 }
-];
+import { courses } from './courses';
 
 const App: React.FC = () => {
 	const [selectedCourses, setSelectedCourses] = useState<Course[]>(() => {
